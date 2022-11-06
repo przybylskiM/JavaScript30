08a0a1ec27a3f4fe936e8859c02f001068cfed68
Maciej Przybylski <maciejprzybylski93@gmail.com>
2022-11-06 21:18:51 +0100

    DrumKit: Build logic

---
+++ DrumKit/main.js
@@ -0,0 +1,20 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+    if (!audio) return; //stop the function from running all together 
+    key.classList.add('playing');
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+
+window.addEventListener('keydown', playSound);
